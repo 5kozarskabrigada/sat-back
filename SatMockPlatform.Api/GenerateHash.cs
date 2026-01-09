14aa9360d1d1baddfe5a19c41078a31bd5fa8746
Tomris Mammadova <mammadova.tomris@gmail.com>
2026-01-09 21:07:23 +0400

    Remove redundant GenerateHash.cs

--- SatMockPlatform.Api/GenerateHash.cs
+++
@@ -1,6 +0,0 @@
-using BCrypt.Net;
-
-var password = "admin123";
-var hash = BCrypt.Net.BCrypt.HashPassword(password);
-Console.WriteLine($"Password: {password}");
-Console.WriteLine($"Hash: {hash}");
